8218983d47193736f0113214766ee1a522410752
Amor <amore.acecalrgmail.com>
2023-03-16 13:56:15 +0100

    1º commit agrefamos la estructura, los atributos y el constructor

---
+++ Profesion.java
@@ -0,0 +1,25 @@
+/**
+ * Write a description of class Profesion here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Profesion
+{
+    // instance variables - replace the example below with your own
+    private String nombreProfesion;
+    
+    private String Rango;
+    /**
+     * Constructor for objects of class Profesion
+     */
+    public Profesion()
+    {
+
+    }
+
+    
+
+
+
+}
